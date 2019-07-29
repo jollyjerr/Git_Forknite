6bb6899418e0cbc2e12329588a172df0c684f856
jollyjerr <jollyjerr@gmail.com>
2019-07-29 17:41:05 -0600

    establish login rules

--- lib/cli.rb
+++ lib/cli.rb
@@ -4,6 +4,18 @@ number_of_players = nil
 def main_menu
     system("clear")
     Screen.welcome
-    user_input = gets.chomp.clamp(2, 5)
-    number_of_players = user_input
+    user_input = gets.chomp.to_i
+    case user_input
+    when 2..5
+        number_of_players = user_input
+    else
+        puts "                                      Your choices are:"
+        puts "                                        2 | 3 | 4 | 5"
+        sleep(3)
+        main_menu
+    end 
+end
+
+def create_player
+    Screen.new_player
 end
