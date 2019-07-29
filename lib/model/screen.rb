ecece1f7e1969fb6b9b513460ae21a263ab6c7ce
jollyjerr <jollyjerr@gmail.com>
2019-07-29 12:46:23 -0600

    first screen setup

---
+++ lib/model/screen.rb
@@ -0,0 +1,21 @@
+class Screen
+    def self.two_full_full
+        # system("clear")
+        puts ''
+        puts '                                                             100/100             100/100                                    '
+        puts ''
+        puts '                                                               / \                 / \                                      '
+        puts '                                                              |   |               |   |                                     '
+        puts '                                                               \ /                 \ /                                      '
+        puts '                                                                |                   |                                       '
+        puts '                                                              / | \               / | \                                     '
+        puts '                                                             /  |  \             /  |  \                                    '
+        puts '                                                                |                   |                                       '
+        puts '                                                               / \                 / \                                      '
+        puts '                                                              /   \               /   \                                     '
+        puts '                                                             /     \             /     \                                    '
+        puts '                                        ------------------------------------------------------------------------------------'
+    end
+end
+
+Screen.two_full_full
