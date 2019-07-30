aaf92ddf18c8e52815fb3da6f2006d3512ba78b8
jollyjerr <jollyjerr@gmail.com>
2019-07-30 14:34:22 -0600

    break end of game

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,5 +1,5 @@
 require_all './lib'
-Numbers_to_name = {5 => "five", 4 => "four", 3 => "three", 2 => "two"}
+Numbers_to_name = {5 => "five", 4 => "four", 3 => "three", 2 => "two", 1 => "one"}
 
 
 def main_menu
@@ -18,9 +18,10 @@ def main_menu
     end    
     system("clear")
     number_of_players.times {|player| create_player}
-    match(number_of_players)
+    match
 end
 
+# PLAYER CREATION
 def create_player
     Screen.new_player
     player_name = gets.chomp
@@ -45,26 +46,48 @@ def select_weapons(player)
     sleep(0.5)
 end
 
-def match(players)
-    number_of_players = players
+#PLAY GAME
+def match
+    kill_players
+    number_of_players = Player.all.count
     system("clear")
     Screen.send(Numbers_to_name[number_of_players])
     if number_of_players > 2
-        Player.all.each {|player| turn_many(player)}
+        Player.all.each do |player| 
+            turn_many(player)
+        end
+        match
+    elsif number_of_players == 2
+        Player.all.each do |player|
+            if player.health < 1
+                puts "run game_over here"
+            end
+            puts "Your health is #{player.health}"
+            turn_duel(player)
+        end
+        match
     else
-        Player.all.each {|player| turn_duel(player)}
+        puts "run game_over here"
+        sleep(5)
     end
 end
 
 def turn_duel(player)
-    attack(player)
+    target = Player.all.select {|target| target.name != player.name}[0]
+    target.health -= attack(player)
+    target.save
+    puts target.health
+    sleep(2)
 end
 
 def turn_many(player)
     puts "                                        #{player.name}, choose a target!"
     input = gets.chomp
-    target = Player.all.select {|target| target.name == input || target.id == input}
-    attack(player)
+    target = Player.all.select {|target| target.name == input || target.id == input}[0]
+    target.health -= attack(player)
+    target.save
+    puts target.health
+    sleep(2)
 end
 
 def attack(player)
@@ -72,7 +95,16 @@ def attack(player)
     puts ''
     player.weapons.each {|weapon| puts "                                        #{weapon.name}"}
     weapon_choice = gets.chomp
-    weapon = player.weapons.select {|weapon| weapon.name == weapon_choice}
+    weapon = player.weapons.select {|weapon| weapon.name == weapon_choice}[0]
+    weapon.damage
+end
+
+def kill_players
+    Player.all.each do |player|
+        if player.health <= 0
+            player.destroy
+        end
+    end
 end
 
 
