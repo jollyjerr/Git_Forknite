a198ef381d8a755a670dc3b2523f588fc6421c28
jollyjerr <jollyjerr@gmail.com>
2019-07-31 12:55:46 -0600

    create functional menus

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,4 +1,6 @@
 require_all './lib'
+require 'tty-prompt'
+Prompt = TTY::Prompt.new
 Numbers_to_name = {5 => "five", 4 => "four", 3 => "three", 2 => "two", 1 => "one"}
 
 
@@ -24,7 +26,7 @@ end
 # PLAYER CREATION
 def create_player
     Screen.new_player
-    player_name = gets.chomp
+    player_name = gets.chomp.to_str
     new_player = Player.create(name: player_name)
     system("clear")
     select_weapons(new_player)
@@ -35,14 +37,12 @@ def select_weapons(player)
     puts ''
     puts "Welcome #{player.name}!".center(95)
     Screen.weapon_selection
-    Weapon.all.each {|weapon| puts "                                        #{weapon.name}" }
-    weapon_one_choice = gets.chomp
-    player.weapons << Weapon.all.select {|weapon| weapon.name == weapon_one_choice}
-    puts "#{weapon_one_choice} equipped!"
-    weapon_two_choice = gets.chomp
-    player.weapons << Weapon.all.select {|weapon| weapon.name == weapon_two_choice}
-    puts "#{weapon_two_choice} equipped!"
-    # puts "#{player.weapons.map {|weapon| weapon.name}}" #TEST CODE FOR CHECKING WEAPON OWNERSHIP
+    choices = Weapon.all.map {|weapon| weapon.name}
+    weapon_choices = Prompt.multi_select("", choices, max: 2, default: 2, per_page: 10)
+    weapon_one = weapon_choices[0]
+    weapon_two = weapon_choices[1]
+    player.weapons << Weapon.all.select {|weapon| weapon.name == weapon_one}
+    player.weapons << Weapon.all.select {|weapon| weapon.name == weapon_two}
     sleep(0.5)
 end
 
@@ -80,11 +80,10 @@ end
 def turn_duel(player)
     live_players = Player.all.select {|player| player.health > 0}
     target = live_players.select {|target| target.name != player.name}[0]
-    puts target.name
     target.health -= attack(player)
     target.save
     puts target.health
-    sleep(2)
+    sleep(0.5)
 end
 
 def many_checker(player)
@@ -95,19 +94,21 @@ end
 
 def turn_many(player)
     puts "                                        #{player.name}, choose a target!"
-    input = gets.chomp
+    live_players = Player.all.select {|player| player.health > 0}
+    choices = live_players.map {|player| player.name}
+    input = Prompt.select("", choices)
     target = Player.all.select {|target| target.name == input || target.id == input}[0]
     target.health -= attack(player)
     target.save
     puts target.health
-    sleep(2)
+    sleep(0.5)
 end
 
 def attack(player)
     puts "                                        #{player.name}, choose a weapon!"
     puts ''
-    player.weapons.each {|weapon| puts "                                        #{weapon.name}"}
-    weapon_choice = gets.chomp
+    choices = player.weapons.map {|weapon| weapon.name}
+    weapon_choice = Prompt.select("", choices)
     weapon = player.weapons.select {|weapon| weapon.name == weapon_choice}[0]
     weapon.damage
 end
